6850e849532ca0765eebae7f1e9d569b6bf2da4c
Ilia Makhonin <noizemkutane77@gmail.com>
2018-12-08 00:23:03 +0300

    Create reduser for treatment message

--- src/app/reducers/messages.js
+++ src/app/reducers/messages.js
@@ -3,14 +3,17 @@ import constants from '../constants/constants';
 const messagesReducer = (state = [], action) => {
 	switch(action.type) {
 		case constants.RECEIVE_NEW_MESSAGE:
-			return state.concat({
-				text: action.text,
-				author: action.author,
-				time: action.time
-			});
+			return [
+				...state,
+				{
+					text: action.text,
+					author: action.author,
+					time: action.time
+				}
+			];
 		default:
 			return state
 	}
 };
 
-export default messagesReducer
+export default messagesReducer;

70ad1285c177ee0ab48154193253a2403a980104
Ilia Makhonin <noizemkutane77@gmail.com>
2018-12-08 00:21:49 +0300

    Add reducers at combineReducers function

--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
+import messagesReducer from './messages';
+import peopleReducer from './peoples';
+
 
 const mainReducer = combineReducers({
-	//
+	messagesReducer,
+	peopleReducer
 });
 
 export default mainReducer;

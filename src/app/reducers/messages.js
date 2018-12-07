eeb33ebba926e8582df3facdec49c9a18ace27c3
Ilia Makhonin <noizemkutane77@gmail.com>
2018-12-07 23:58:34 +0300

    Create messages reduser

---
+++ src/app/reducers/messages.js
@@ -0,0 +1,16 @@
+import constants from '../constants/constants';
+
+const messagesReducer = (state = [], action) => {
+	switch(action.type) {
+		case constants.RECEIVE_NEW_MESSAGE:
+			return state.concat({
+				text: action.text,
+				author: action.author,
+				time: action.time
+			});
+		default:
+			return state
+	}
+};
+
+export default messagesReducer

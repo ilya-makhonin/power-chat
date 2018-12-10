f2662fcfb26bfc060b9f87f3e6f4f9875ab372b6
Ilia Makhonin <noizemkutane77@gmail.com>
2018-12-10 23:35:06 +0300

    Modified App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import ChatWrap from './app/containers/ChatWrap';
+import store from './app/store/store';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello React!</h1>
-      </div>
-    );
-  }
+
+export default class App extends Component {
+	render() {
+		return (
+            <Provider key={ module.hot ? Date.now() : store} store={store}>
+              <div className="container clearfix">
+                <ChatWrap/>
+              </div>
+            </Provider>
+		);
+	}
 }
-
-export default App;

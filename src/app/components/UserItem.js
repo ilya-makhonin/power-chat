655351384af67e4a8a9476fe98f822d454e92bea
Ilia Makhonin <noizemkutane77@gmail.com>
2018-12-12 00:10:42 +0300

    Code stylization. Used arrow function

--- src/app/components/UserItem.js
+++ src/app/components/UserItem.js
@@ -1,20 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class UserItem extends Component {
-	render() {
-		return (
-			<li className="clearfix">
-				<img
-					src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/chat_avatar_01.jpg"
-					alt="avatar"
-				/>
-				<div className="about">
-					<div className="name">{ this.props.userName }</div>
-					<div className="status">
-						<i className="fa fa-circle online"/> online
-					</div>
+
+const UserItem = ({ userName }) => {
+	return (
+		<li className="clearfix">
+			<img
+				src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/chat_avatar_01.jpg"
+				alt="avatar"
+			/>
+			<div className="about">
+				<div className="name">{ userName }</div>
+				<div className="status">
+					<i className="fa fa-circle online"/> online
 				</div>
-			</li>
-		);
-	}
-}
+			</div>
+		</li>
+	);
+};
+
+export default UserItem;
+
+
+// export default class UserItem extends Component {
+// 	render() {
+// 		return (
+// 			<li className="clearfix">
+// 				<img
+// 					src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/chat_avatar_01.jpg"
+// 					alt="avatar"
+// 				/>
+// 				<div className="about">
+// 					<div className="name">{ this.props.userName }</div>
+// 					<div className="status">
+// 						<i className="fa fa-circle online"/> online
+// 					</div>
+// 				</div>
+// 			</li>
+// 		);
+// 	}
+// }

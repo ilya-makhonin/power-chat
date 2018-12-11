e606b155ea3f08d5d041f3c216846f99b8504f62
Ilia Makhonin <noizemkutane77@gmail.com>
2018-12-12 00:11:59 +0300

    Code modified. Use arrow function for generate markup

--- src/app/components/ChatHeader.js
+++ src/app/components/ChatHeader.js
@@ -1,14 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class ChatHeader extends Component {
-	render() {
-		return (
-			<div className="chat-header clearfix">
-				<div className="chat-about">
-					<div className="chat-with">MainHall</div>
-				</div>
-				<i className="fa fa-star"/>
-			</div>
-		);
-	}
-}
+
+const ChatHeader = () => (
+	<div className="chat-header clearfix">
+		<div className="chat-about">
+			<div className="chat-with">MainHall</div>
+		</div>
+		<i className="fa fa-star"/>
+	</div>
+);
+
+export default ChatHeader;
+
+
+// export default class ChatHeader extends Component {
+// 	render() {
+// 		return (
+// 			<div className="chat-header clearfix">
+// 				<div className="chat-about">
+// 					<div className="chat-with">MainHall</div>
+// 				</div>
+// 				<i className="fa fa-star"/>
+// 			</div>
+// 		);
+// 	}
+// }

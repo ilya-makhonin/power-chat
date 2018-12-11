a12793484ccbca8aeadfeadfb39e3fba3fcb7f61
Ilia Makhonin <noizemkutane77@gmail.com>
2018-12-12 00:09:02 +0300

    Cod stylization. Used arrow function

--- src/app/components/MessageItem.js
+++ src/app/components/MessageItem.js
@@ -1,19 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class MessageItem extends Component {
-	render() {
-		// console.log(this.props);
-		return (
-			<li className="clearfix">
-				<div className="message-data align-right">
-					<span className="message-data-time">10:10 AM, Today</span> &nbsp; &nbsp;
-					<span className="message-data-name">Olia</span>
-					<i className="fa fa-circle me"/>
-				</div>
-				<div className="message other-message float-right">
-					Hi Vincent, how are you? How is the project coming along?
-				</div>
-			</li>
-		);
-	}
-}
+
+const MessageItem = (props) => {
+	console.log(props);
+	return (
+		<li className="clearfix">
+			<div className="message-data align-right">
+				<span className="message-data-time">10:10 AM, Today</span> &nbsp; &nbsp;
+				<span className="message-data-name">Olia</span>
+				<i className="fa fa-circle me"/>
+			</div>
+			<div className="message other-message float-right">
+				Hi Vincent, how are you? How is the project coming along?
+			</div>
+		</li>
+	);
+};
+
+export default MessageItem;
+
+
+// export default class MessageItem extends Component {
+// 	render() {
+// 		// console.log(this.props);
+// 		return (
+// 			<li className="clearfix">
+// 				<div className="message-data align-right">
+// 					<span className="message-data-time">10:10 AM, Today</span> &nbsp; &nbsp;
+// 					<span className="message-data-name">Olia</span>
+// 					<i className="fa fa-circle me"/>
+// 				</div>
+// 				<div className="message other-message float-right">
+// 					Hi Vincent, how are you? How is the project coming along?
+// 				</div>
+// 			</li>
+// 		);
+// 	}
+// }

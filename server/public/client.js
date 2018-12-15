845b0495d46c3c77f7f3326f16dc050ebaec38eb
Ilia Makhonin <noizemkutane77@gmail.com>
2018-12-15 23:55:04 +0300

    Script for client side

---
+++ server/public/client.js
@@ -0,0 +1,41 @@
+$(function () {
+    let content = $('#content');
+    let status = $('#status');
+    let input = $('#input');
+    let myColor = false;
+    let myName = false;
+    let connection = new WebSocket('ws:' + window.location.href.substring(window.location.protocol.length));
+
+    connection.onopen = function () {
+        status.text('Choose name:');
+    };
+    connection.onmessage = function (message) {
+        let json = JSON.parse(message.data);
+        if (json.type === 'color') { // first response from the server with user's color
+            myColor = json.data;
+            status.text(myName + ': ').css('color', myColor);
+        }else if (json.type === 'message') { // it's a single message
+            addMessage(json.data.author, json.data.text,
+                json.data.color, new Date(json.data.time));
+        }
+    };
+
+    input.keydown(function(e) {
+        if (e.keyCode === 13) {
+            let msg = $(this).val();
+            if (!msg)    return;
+            connection.send(msg);
+            $(this).val('');
+            if (myName === false) {
+                myName = msg;
+            }
+        }
+    });
+
+
+    function addMessage(author, message, color, dt) {
+        content.append('<p><span style="color:' + color + '">' + author + '</span> @ ' +
+            + dt.getHours() + ':'
+            + (dt.getMinutes()<10?('0'+ dt.getMinutes()):dt.getMinutes()) + '----' + message + '</p>');
+    }
+});

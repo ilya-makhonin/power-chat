83656611c8c97296836a3f6866170b88578f3fdc
Ilia Makhonin <noizemkutane77@gmail.com>
2018-12-17 23:13:16 +0300

    Install morgan. Instead app.logger

--- server/main.js
+++ server/main.js
@@ -66,10 +66,10 @@ wss.on('connection', function(ws) {
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
+// app.use(express.favicon());
+// app.use(express.logger('dev'));
+// app.use(express.bodyParser());
+// app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
